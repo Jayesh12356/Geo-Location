89e6a053842cc9960b944de87eb9a6c545b0907c
Jayesh Koli <Jayesh.Koli@ril.com>
2024-07-08 19:34:11 +0530

    Geo Location

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import { useState, StrictMode } from "react";
+import { useGeolocation } from "./useGeoLocation";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+export default function App() {
+  const {
+    isLoading,
+    position: { lat, lng },
+    error,
+    getPosition,
+  } = useGeolocation();
+
+  const [countClicks, setCountClicks] = useState(0);
+
+  // const { lat, lng } = position;
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
